7d9f1a69059434677c4a9a217d68c8780cf6effd
Stephen Moss <bladexr8@gmail.com>
2021-03-25 20:56:52 +1100

    finished first microservice

--- ch02/first-microservice/src/common/logging.ts
+++ ch02/first-microservice/src/common/logging.ts
@@ -6,7 +6,7 @@
  * Date: 25/03/2021
  * *************************************************************** */
 
-const winston = require('winston');
+import * as winston from 'winston';
 
 /*
  * Level specifies message level. Logger will only output messages of
@@ -22,23 +22,28 @@ const winston = require('winston');
  * 5 silly
  */
 
-// custom log format
-const logFormat = winston.format.combine(
-  winston.format.colorize(),
-  winston.format.timestamp(),
-  winston.format.align(),
-  winston.format.printf(
-    // eslint-disable-next-line arrow-parens
-    (info: any): string => `${info.timestamp} ${info.level}: ${info.message}`,
-  ),
-);
+ export class appLogger {
 
-// create the logger
-const logger = winston.createLogger({
-  level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-  // format: winston.format.combine(winston.format.splat(), winston.format.simple()),
-  format: logFormat,
-  transports: [new winston.transports.Console()],
-});
+  logFormat: any;
 
-module.exports = logger;
+  logger: winston.Logger;
+
+  constructor() {
+    this.logFormat = winston.format.combine(
+      winston.format.colorize(),
+      winston.format.timestamp(),
+      winston.format.align(),
+      winston.format.printf(
+        // eslint-disable-next-line arrow-parens
+        (info: any): string => `${info.timestamp} ${info.level}: ${info.message}`,
+      ),
+    );
+
+    this.logger = winston.createLogger({
+      level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
+      // format: winston.format.combine(winston.format.splat(), winston.format.simple()),
+      format: this.logFormat,
+      transports: [new winston.transports.Console()],
+    });
+  }
+}
--- ch02/first-microservice/src/config/Application.ts
+++ ch02/first-microservice/src/config/Application.ts
@@ -1,2 +1,19 @@
-import { logger } from '../common/logging'
+import { appLogger } from '../common/logging';
+import { ExpressConfig } from './Express';
+
+export class Application {
+    server: any;
+    express: ExpressConfig;
+    classLogger: appLogger;
+
+    constructor() {
+        this.express = new ExpressConfig();
+        this.classLogger = new appLogger();
+
+        const port = 3000;
+        this.server = this.express.app.listen(port, () => {
+            this.classLogger.logger.info(`Server Started! Express: http://localhost:${port}`);
+        });
+    }
+}
 
